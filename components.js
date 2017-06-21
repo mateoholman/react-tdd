29b59ae862b74b599f7ff415f18f055c2f78c940
Matthew Holman <mateoholman@gmail.com>
2017-06-21 16:14:36 -0500

    Added test & spec for passing a prop to InputArea

--- components.js
+++ components.js
@@ -17,7 +17,7 @@ export class BeerListContainer extends Component {
   render() {
     return (
       <div>
-        <InputArea />
+        <InputArea onSubmit={this.addItem} />
         <BeerList />
       </div>
     );
@@ -29,6 +29,10 @@ export class InputArea extends Component {
     return <input/>
   }
 }
+
+InputArea.PropTypes = {
+  onSubmit: React.PropTypes.func.isRequired
+};
 
 export class BeerList extends Component {
   render() {
